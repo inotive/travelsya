684bbbb95de79c029e4c8653596d596603820f76
esi asyani <agiltriyasmoko@gmail.com>
2023-06-05 04:40:30 +0700

    fixed:callbac4

--- app/Http/Controllers/API/CallbackController.php
+++ app/Http/Controllers/API/CallbackController.php
@@ -102,7 +102,7 @@ class CallbackController extends Controller
                         }
                         $point->deductPoint($transaction->user_id, abs($feepoint), $transaction->id);
                     } else {
-                        $point->addPoint($transaction->user_id, $data['paid_amount'], $transaction->id, $transaction->servvice_id);
+                        $point->addPoint($transaction->user_id, $data['paid_amount'], $transaction->id, $transaction->service_id);
                     }
                 } else {
 
