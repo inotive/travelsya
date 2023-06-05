8da8dba7f0f7ead785c1905dac5436eb96cf936d
Agung Kurniawan <agung41149@gmail.com>
2023-06-05 20:34:12 +0700

    add new style for mobile icon

--- resources/views/layouts/include/home/menu-bar.blade.php
+++ resources/views/layouts/include/home/menu-bar.blade.php
@@ -11,6 +11,7 @@
     align-items: center;
     width: 72px;
     height: 72px;
+    margin: 0 auto;
   }
   .item-label {
     flex: 1;
@@ -21,6 +22,14 @@
     text-overflow: ellipsis;
     overflow: hidden;
     width: 100%;
+    text-align: left;
+    margin-left: 1.2em;
+  }
+  @media (max-width: 767px) {
+    .item-label {
+      margin-left: 0px;
+      text-align: center;
+    }
   }
 </style>
 @endpush
@@ -28,48 +37,76 @@
   <div id="" class="container-xl bg-transparent">
     <div class="card">
       <div class="card-body">
-        <div class="row g-3">
-          <a href="/" class="col-6 col-md-3 col-lg-2 col-sm-6 d-flex align-items-center">
-            <div class="child-item-menubar">
-              <img src="{{asset('assets/media/products-categories/icon-hotel.png')}}" class="w-40px" />
+        <div class="row gy-3">
+          <a href="/" class="col-4 col-md-3 col-lg-2 align-items-center">
+            <div class="row">
+              <div class="col-md-4 col-sm-12 justify-content-center">
+                <div class="child-item-menubar">
+                  <img src="{{asset('assets/media/products-categories/icon-hotel.png')}}" class="w-40px" />
+                </div>
+              </div>
+              <span class="col-md-8 col-sm-12 text-red fw-bold fs-6 item-label">Hotel</span>
             </div>
-            <span class="text-danger fw-bold fs-6 item-label">Hotel</span>
           </a>
-          <a href="/" class="col-6 col-md-3 col-lg-2 col-sm-6 d-flex align-items-center">
-            <div class="child-item-menubar">
-              <img src="{{asset('assets/media/products-categories/icon-hostel.png')}}" class="w-40px" />
+          <a href="/" class="col-4 col-md-3 col-lg-2 align-items-center">
+            <div class="row">
+              <div class="col-md-4 col-sm-12 justify-content-center">
+                <div class="child-item-menubar">
+                  <img src="{{asset('assets/media/products-categories/icon-hostel.png')}}" class="w-40px" />
+                </div>
+              </div>
+              <span class="col-md-8 col-sm-12 text-gray-600 fw-bold fs-6 item-label">Hostel</span>
             </div>
-            <span class="text-gray-800 fw-bold fs-6 item-label">Hostel</span>
           </a>
-          <a href="/" class="col-6 col-md-3 col-lg-2 col-sm-6 d-flex align-items-center">
-            <div class="child-item-menubar">
-              <img src="{{asset('assets/media/products-categories/icon-bpjs.png')}}" class="w-30px" />
+          <a href="/" class="col-4 col-md-3 col-lg-2 align-items-center">
+            <div class="row">
+              <div class="col-md-4 col-sm-12 justify-content-center">
+                <div class="child-item-menubar">
+                  <img src="{{asset('assets/media/products-categories/icon-bpjs.png')}}" class="w-30px" />
+                </div>
+              </div>
+              <span class="col-md-8 col-sm-12 text-gray-600 fw-bold fs-6 item-label">BPJS</span>
             </div>
-            <span class="text-gray-800 fw-bold fs-6 item-label">BPJS</span>
           </a>
-          <a href="/" class="col-6 col-md-3 col-lg-2 col-sm-6 d-flex align-items-center">
-            <div class="child-item-menubar">
-              <img src="{{asset('assets/media/products-categories/icon-pajak.png')}}" class="w-30px" />
+          <a href="/" class="col-4 col-md-3 col-lg-2 align-items-center">
+            <div class="row">
+              <div class="col-md-4 col-sm-12 justify-content-center">
+                <div class="child-item-menubar">
+                  <img src="{{asset('assets/media/products-categories/icon-pajak.png')}}" class="w-30px" />
+                </div>
+              </div>
+              <span class="col-md-8 col-sm-12 text-gray-600 fw-bold fs-6 item-label">Pajak</span>
             </div>
-            <span class="text-gray-800 fw-bold fs-6 item-label">Pajak</span>
           </a>
-          <a href="/" class="col-6 col-md-3 col-lg-2 col-sm-6 d-flex align-items-center">
-            <div class="child-item-menubar">
-              <img src="{{asset('assets/media/products-categories/icon-pdam.png')}}" class="w-40px" />
+          <a href="/" class="col-4 col-md-3 col-lg-2 align-items-center">
+            <div class="row">
+              <div class="col-md-4 col-sm-12 justify-content-center">
+                <div class="child-item-menubar">
+                  <img src="{{asset('assets/media/products-categories/icon-pdam.png')}}" class="w-40px" />
+                </div>
+              </div>
+              <span class="col-md-8 col-sm-12 text-gray-600 fw-bold fs-6 item-label">PDAM</span>
             </div>
-            <span class="text-gray-800 fw-bold fs-6 item-label">PDAM</span>
           </a>
-          <a href="/" class="col-6 col-md-3 col-lg-2 col-sm-6 d-flex align-items-center">
-            <div class="child-item-menubar">
-              <img src="{{asset('assets/media/products-categories/icon-pulsa.png')}}" class="w-40px" />
+          <a href="/" class="col-4 col-md-3 col-lg-2 align-items-center">
+            <div class="row">
+              <div class="col-md-4 col-sm-12 justify-content-center">
+                <div class="child-item-menubar">
+                  <img src="{{asset('assets/media/products-categories/icon-pulsa.png')}}" class="w-40px" />
+                </div>
+              </div>
+              <span class="col-md-8 col-sm-12 text-gray-600 fw-bold fs-6 item-label">Pulsa & Data</span>
             </div>
-            <span class="text-gray-800 fw-bold fs-6 item-label">Pulsa & Data</span>
           </a>
-          <a href="/" class="col-6 col-md-3 col-lg-2 col-sm-6 d-flex align-items-center">
-            <div class="child-item-menubar">
-              <img src="{{asset('assets/media/products-categories/icon-tv.png')}}" class="w-30px" />
+          <a href="/" class="col-4 col-md-3 col-lg-2 align-items-center">
+            <div class="row">
+              <div class="col-md-4 col-sm-12 justify-content-center">
+                <div class="child-item-menubar">
+                  <img src="{{asset('assets/media/products-categories/icon-tv.png')}}" class="w-30px" />
+                </div>
+              </div>
+              <span class="col-md-8 col-sm-12 text-gray-600 fw-bold fs-6 item-label">TV Berbayar</span>
             </div>
-            <span class="text-gray-800 fw-bold fs-6 item-label">TV Berbayar</span>
           </a>
         </div>
       </div>
