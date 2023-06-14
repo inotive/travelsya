d637fc9279cdd79e930a7830ae11fe557da6067b
esi asyani <agiltriyasmoko@gmail.com>
2023-06-14 07:10:06 +0700

    fixed: dashboard admin2

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -112,7 +112,7 @@
                                             <td>{{$transaction->no_inv}}</td>
                                             <td>{{$transaction->req_id}}</td>
                                             <td>{{$transaction->user->name}}</td>
-                                            @if(isset($transaction['bookDate']))
+                                            @if(isset($transaction->bookDate))
                                             <td>{{$transaction->bookDate[0]->start}}</td>
                                             <td>{{$transaction->bookDate[0]->end}}</td>
                                             @else
@@ -135,8 +135,8 @@
                         @php
                         $catId = $service->id;
                         $filterTransactions = array_filter($transactions->toArray(),function($val) use ($catId){
-                        return ($val['service_id'] == $catId);
-                        });
+                        return ($val['service_id'] == 7);
+                    });
                         @endphp
                         <div class="tab-pane fade" id="kt_tab_pane_{{$key}}" role="tabpanel">
                             <div class="table-responsive">
@@ -158,13 +158,14 @@
 
                                     @forelse($filterTransactions as $key2 => $transaction)
                                         <tr>
+                                            {{-- {{print_r($transaction['book_date'])}} --}}
                                             <td>{{date('d/m/y',strtotime($transaction["created_at"]))}}</td>
                                             <td>{{$transaction['no_inv']}}</td>
                                             <td>{{$transaction['req_id']}}</td>
                                             <td>{{$transaction['user']['name']}}</td>
-                                            @if(isset($transaction['bookDate']))
-                                            <td>{{$transaction['bookDate'][0]['start']}}</td>
-                                            <td>{{$transaction['bookDate'][0]['end']}}</td>
+                                            @if(isset($transaction['book_date']))
+                                            <td>{{$transaction['book_date'][0]['start']}}</td>
+                                            <td>{{$transaction['book_date'][0]['end']}}</td>
                                             @else
                                             <td></td>
                                             <td></td>
