e4333bd3cbbc733ab4457c0c3019e2feb8f24b16
gusbgs <gusbagusws@gmail.com>
2023-09-19 23:00:19 +0800

    test github

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -6,7 +6,7 @@
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 
 <head>
-    <title>Traveslya - @yield('title')</title>
+    <title>Traveslya - Halo</title>
     <meta charset="utf-8" />
     <meta name="description" content="" />
     <meta name="keywords" content="" />
