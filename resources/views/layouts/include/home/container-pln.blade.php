c35288f531c11e28d536aaaf6d3aef30187d8b25
Muhammad Irfan Permana <irfanthejoelax@gmail.com>
2023-10-02 09:17:51 +0800

    PTTW-56: [Website Landing] Halaman PLN - Inquiry Tagihan - Integrasi Mili

--- resources/views/layouts/include/home/container-pln.blade.php
+++ resources/views/layouts/include/home/container-pln.blade.php
@@ -3,7 +3,8 @@
     <div class="col-xl-12">
 
         <!--begin::Tiles Widget 2-->
-        <form action="{{ route('product.payment.pln') }}" method="GET" class="card bgi-no-repeat bgi-size-contain card-xl-stretch mb-xl-8 container-xxl mb-5">
+        <form action="{{ route('product.payment.pln') }}" method="GET"
+            class="card bgi-no-repeat bgi-size-contain card-xl-stretch mb-xl-8 container-xxl mb-5">
             <!--begin::Body-->
             <div class="card-body d-flex flex-column justify-content-between">
                 <!--begin::Title-->
@@ -16,8 +17,9 @@
                         </label>
 
                         <input type="text" id="noPelangganPLN" class="form-control form-control-lg"
-                               name="noPelangganPLN" placeholder="Masukan nomor pelanggan" value=""/>
-                               <small class="text-danger" style="display: none" id="textAlert">No. Pelanggan harus terisi</small>
+                            name="noPelangganPLN" placeholder="Masukan nomor pelanggan" value="" />
+                        <small class="text-danger" style="display: none" id="textAlert">No. Pelanggan harus
+                            terisi</small>
 
                         <input type="hidden" name="namaPelanggan" id="inputNamaPelangganPLN">
                         <input type="hidden" name="totalTagihan" id="inputTotalTagihanPLN">
@@ -25,47 +27,54 @@
                         <input type="hidden" name="totalBayar" id="inputTotalBayarPLN">
                     </div>
                     <div class="col-4">
-                        {{-- <button type="button" class="btn btn-danger mt-8 w-100" id="btnPeriksaPLN">Periksa</button> --}}
-                        @auth
                         <button type="button" class="btn btn-danger mt-8 w-100" id="btnPeriksaPLN">Periksa</button>
-                        @endauth
                     </div>
-                    <div class="col-12">
-                        <label class="fs-5 fw-semibold my-3">
-                            <span>Detail Pelanggan</span>
-                        </label>
-                        <div class="table-responsive">
-                            <table class="table table-bordered">
-                                <tbody>
-                                <tr class="py-5">
-                                    <td class="bg-light fw-bold fs-6 text-gray-800">Nama Pelanggan</td>
-                                    <td class="text-right" colspan="3"><span id="namaPelangganPLN"></span></td>
-                                </tr>
-                                <tr class="py-5">
-                                    <td class="bg-light fw-bold fs-6 text-gray-800">Total Tagihan</td>
-                                    <td>Rp. <span id="totalTagihanPLN"></span></td>
-                                    <td class="bg-light fw-bold fs-6 text-gray-800">Biaya Admin</td>
-                                    <td>Rp. <span id="biayaAdminPLN"></span></td>
-                                </tr>
-                                <tr class="py-5">
-                                    <td class="bg-light fw-bold fs-6 text-gray-800">Total Bayar</td>
-                                    <td colspan="2">Rp. <span id="totalBayarPLN"></span></td>
-                                </tr>
-                                </tbody>
-                            </table>
+
+                    <div class="row mt-4" id="detailPLN">
+                        <div class="col-12">
+                            <label class="fs-5 fw-semibold my-3">
+                                <span>Detail Pelanggan</span>
+                            </label>
+                            <div class="table-responsive">
+                                <table class="table table-bordered">
+                                    <tbody>
+                                        <tr class="py-5">
+                                            <td class="bg-light fw-bold fs-6 text-gray-800">Nama Pelanggan</td>
+                                            <td class="text-right" colspan="3"><span id="namaPelangganPLN"></span></td>
+                                        </tr>
+                                        <tr class="py-5">
+                                            <td class="bg-light fw-bold fs-6 text-gray-800">Total Tagihan</td>
+                                            <td>Rp. <span id="totalTagihanPLN"></span></td>
+                                            <td class="bg-light fw-bold fs-6 text-gray-800">Biaya Admin</td>
+                                            <td>Rp. <span id="biayaAdminPLN"></span></td>
+                                        </tr>
+                                        <tr class="py-5">
+                                            <td class="bg-light fw-bold fs-6 text-gray-800">Total Bayar</td>
+                                            <td colspan="2">Rp. <span id="totalBayarPLN"></span></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
                         </div>
+                        <div class="col-12">
+                            @auth
+                            <button type="submit" class="btn btn-danger w-100" id="btnSubmitPLN"
+                                disabled>Pembayaran</button>
+                            @endauth
 
-                    </div>
-                    <div class="col-12">
-                        @auth
-                            <button type="submit" class="btn btn-danger w-100" id="btnSubmitPLN" disabled>Pembayaran</button>
-                        @endauth
-
-                        @guest
+                            @guest
                             <a href="{{ route('login') }}" class="btn btn-danger w-100">
                                 Login Terlebih Dahulu
                             </a>
-                        @endguest
+                            @endguest
+                        </div>
+                    </div>
+
+                    <div class="row mt-5">
+                        <div class="col-xl-12">
+                            <div id="alertPLN"></div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -78,15 +87,17 @@
 </div>
 
 @push('add-style')
-    {{-- <script src="{{ asset('assets/js/custom/noTelp.js') }}"></script> --}}
+{{-- <script src="{{ asset('assets/js/custom/noTelp.js') }}"></script> --}}
 @endpush
 
 @push('add-script')
-    <script>
-       $(document).ready(function () {
+<script>
+    $(document).ready(function () {
             $('#noPelangganPLN').on('keyup', function () {
                 $('#textAlert').hide();
             });
+
+            $('#detailPLN').hide();
 
             $('#btnPeriksaPLN').on('click', function () {
                 var noPelangganPLN = $('#noPelangganPLN').val();
@@ -95,56 +106,59 @@
                     $('#textAlert').show();
                     return false;
                 }
+                $('#alertPLN').empty()
+                $('#detailPLN').hide();
+                $('#btnPeriksaPLN').attr('disabled', true);
+                $('#btnPeriksaPLN').html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...');
 
                 $.ajax({
                     type: "POST",
                     url: "{{ route('product.pln') }}",
-                    // url: "https://servicevps.travelsya.com/product/pln",
                     data: {
                         'no_pelanggan': noPelangganPLN,
                         'nom': 'CEKPLN',
                     },
                     success: function (response) {
-                        $.ajax({
-                            type: "POST",
-                            url: "{{ route('product.adminFee') }}",
-                            data: {
-                                'idProduct':  459,
-                            },
-                            beforeSend: function() {
-                                $('#btnPeriksaPLN').attr('disabled', true);
-                                $('#btnPeriksaPLN').html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...');
-                            },
-                            success: function (responseTagihan) {
+                        var simulateFeePLN = parseInt(response.data.fee);
 
-                                var simulateFeePLN = parseInt(responseTagihan[0].value);
+                        var simulateAmountPLN = parseInt(response.data.tagihan);
+                        var simulateTotalPLN = simulateAmountPLN + simulateFeePLN;
 
-                                var simulateAmountPLN = parseInt(response.data.tagihan);
-                                var simulateTotalPLN = simulateAmountPLN + simulateFeePLN;
+                        $('#namaPelangganPLN').text(response.data.nama_pelanggan);
+                        $('#totalTagihanPLN').text(new Intl.NumberFormat('id-ID').format(simulateAmountPLN));
+                        $('#biayaAdminPLN').text(new Intl.NumberFormat('id-ID').format(simulateFeePLN));
+                        $('#totalBayarPLN').text(new Intl.NumberFormat('id-ID').format(simulateTotalPLN));
 
-                                $('#namaPelangganPLN').text(response.data.nama_pelanggan);
-                                $('#totalTagihanPLN').text(new Intl.NumberFormat('id-ID').format(simulateAmountPLN));
-                                $('#biayaAdminPLN').text(new Intl.NumberFormat('id-ID').format(simulateFeePLN));
-                                $('#totalBayarPLN').text(new Intl.NumberFormat('id-ID').format(simulateTotalPLN));
+                        $('#inputNamaPelangganPLN').val(response.data.no_pelanggan);
+                        $('#inputTotalTagihanPLN').val(simulateAmountPLN);
+                        $('#inputBiayaAdminPLN').val(simulateFeePLN);
+                        $('#inputTotalBayarPLN').val(simulateTotalPLN);
 
-                                $('#inputNamaPelangganPLN').val(response.data.no_pelanggan);
-                                $('#inputTotalTagihanPLN').val(simulateAmountPLN);
-                                $('#inputBiayaAdminPLN').val(simulateFeePLN);
-                                $('#inputTotalBayarPLN').val(simulateTotalPLN);
+                        $('#btnPeriksaPLN').removeAttr('disabled');
+                        $('#btnPeriksaPLN').text('Periksa');
 
-                                $('#btnPeriksaPLN').removeAttr('disabled');
-                                $('#btnPeriksaPLN').text('Periksa');
+                        $('#btnSubmitPLN').removeAttr('disabled');
 
-                                $('#btnSubmitPLN').removeAttr('disabled');
-                            }
-                        });
+                        $('#detailPLN').show();
+                    },
+                    error: function(xhr, status, error) {
+                        if (xhr.status === 400 || xhr.status === 500) {
+                            // Buat elemen div dengan kelas 'alert' dan 'alert-danger'
+                            var alertDiv = $(`<div class="alert alert-danger alert-dismissible fade show" role="alert">${xhr.responseJSON.data}<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`);
+
+                            $('#alertPLN').empty().append(alertDiv);
+                        }
+
+                        // Hapus spinner dan aktifkan tombol
+                        $('#btnPeriksaPLN').removeAttr('disabled');
+                        $('#btnPeriksaPLN').text('Periksa');
                     }
                 });
             });
        });
-    </script>
-    {{-- <script>
-        $(document).ready(function() {
+</script>
+{{-- <script>
+    $(document).ready(function() {
             $('#notelp').on('keyup', function(e) {
 
                 $.ajaxSetup({
@@ -191,5 +205,5 @@
             })
 
         })
-    </script> --}}
+</script> --}}
 @endpush
