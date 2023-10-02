b8d109c5068aab8ff34f90a809e6ea34189b86e3
gusbgs <77344072+gusbgs@users.noreply.github.com>
2023-10-02 15:44:53 +0800

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -114,7 +114,7 @@ class ProductController extends Controller
     {
         // return view('product.bpjs');
         $data = $request->all();
-        return $data;
+
         $requestMymili = $this->mymili->inquiry([
             'no_hp' => $data['no_pelanggan'],
             'nom' => $data['nom'],
@@ -487,7 +487,7 @@ class ProductController extends Controller
         // return view('product.tv');
 
         $data = $request->all();
-
+        return $data;
         $requestMymili = $this->mymili->inquiry([
             'no_hp' => $data['no_pelanggan'],
             'nom' => $data['nom'],
