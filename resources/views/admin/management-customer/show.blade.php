047bfb5672a2b3aac52412df34220d213a6d28fd
BhismaAprian <gmdrose123@gmail.com>
2023-10-10 14:25:07 +0800

    [Website Admin] Halaman Admin - Customer - History Transaksi

--- resources/views/admin/management-customer/show.blade.php
+++ resources/views/admin/management-customer/show.blade.php
@@ -19,42 +19,44 @@
             <div class="modal-body scroll-y px-10 px-lg-15 pt-0 pb-15">
                 <!--begin:Form-->
                 {{-- <form id="kt_modal_new_target_form" class="form" action="#"> --}}
-                    <!--begin::Heading-->
-                    <div class="mb-13 text-center">
-                        <!--begin::Title-->
-                        <h1 class="mb-3">History Transaksi - Pelanggan X</h1>
-                        <!--end::Title-->
-                        <!--begin::Description-->
-                        {{-- <div class="text-muted fw-semibold fs-5">If you need more info, please check
+                <!--begin::Heading-->
+                <div class="mb-13 text-center">
+                    <!--begin::Title-->
+                    <h1 class="mb-3">
+                        History Transaksi - Pelanggan <span class="fw-bold nama-customer"></span>
+                    </h1>
+                    <!--end::Title-->
+                    <!--begin::Description-->
+                    {{-- <div class="text-muted fw-semibold fs-5">If you need more info, please check
                             <a href="#" class="fw-bold link-primary">Project Guidelines</a>.
                         </div> --}}
-                        <!--end::Description-->
-                    </div>
-                    <!--end::Heading-->
-                    <div class="card-body py-3">
-                        <!--begin::Table container-->
-                        <div class="table-responsive">
-                            <!--begin::Table-->
-                            <table class="table table-bordered gs-0 gy-4 text-center">
-                                <!--begin::Table head-->
-                                <thead>
-                                <tr class="fw-bold text-muted bg-light">
+                    <!--end::Description-->
+                </div>
+                <!--end::Heading-->
+                <div class="card-body py-3">
+                    <!--begin::Table container-->
+                    <div class="table-responsive">
+                        <!--begin::Table-->
+                        <table class="table table-bordered gs-0 gy-4 text-center" id="kt_datatable_zero_configuration">
+                            <!--begin::Table head-->
+                            <thead>
+                                <tr class="fw-bold text-muted bg-l  ight">
                                     {{-- <th class="ps-4 rounded-start">No</th> --}}
                                     <th class="min-w-125px">Tanggal</th>
                                     <th class="min-w-125px">Invoice</th>
                                     <th class="min-w-50px">Layanan</th>
-                                    <th class="min-w-50px">Custom</th>
                                     <th class="min-w-125px">Deskripsi</th>
                                     <th class="min-w-125px">Metode</th>
                                     <th class="min-w-125px">Grand</th>
-                                    <th class="min-w-125px">Point</th>
+                                    <th class="min-w-70px">Point</th>
                                 </tr>
-                                </thead>
-                                <!--end::Table head-->
-                                <!--begin::Table body-->
-                                <tbody>
-                                {{-- @foreach($customers as $customer) --}}
-                                    <tr>
+                            </thead>
+                            <!--end::Table head-->
+                            <!--begin::Table body-->
+                            <tbody>
+                            <tbody class="fw-semibold text-gray-600" id="dataTbody">
+                                {{-- @foreach ($customers as $customer) --}}
+                                {{-- <tr>
                                         <td>
                                             <div class="text-dark fw-bold  mb-1 fs-6">12 A 123</div>
                                         </td>
@@ -79,19 +81,19 @@
                                         <td>
                                             <div class="text-dark fw-bold  d-block mb-1 fs-6">25 Point</div>
                                         </td>
-                                    </tr>
+                                    </tr> --}}
                                 {{-- @endforeach --}}
-            
-                                </tbody>
-                                <!--end::Table body-->
-                            </table>
-                            {{-- {{$customers->appends(request()->input())->links('vendor.pagination.bootstrap-5')}} --}}
-                            <!--end::Table-->
-                        </div>
-                        <!--end::Table container-->
+
+                            </tbody>
+                            <!--end::Table body-->
+                        </table>
+                        {{-- {{$customers->appends(request()->input())->links('vendor.pagination.bootstrap-5')}} --}}
+                        <!--end::Table-->
                     </div>
-                    <!--begin::Input group-->
-                    <!--end::Actions-->
+                    <!--end::Table container-->
+                </div>
+                <!--begin::Input group-->
+                <!--end::Actions-->
                 {{-- </form> --}}
                 <!--end:Form-->
             </div>
@@ -101,3 +103,68 @@
     </div>
     <!--end::Modal dialog-->
 </div>
+<script>
+    function showDetailTransactions(customerId) {
+    $.ajax({
+        url: 'customer/show', 
+        method: 'GET',
+        data: { customerId: customerId },
+        success: function(response) {
+            
+            // Fill Table Body
+            var tableBody = $('#dataTbody');
+            var data = response;
+            tableBody.empty();
+
+            for (var i = 0; i < data.length; i++) {
+                $('.nama-customer').text(data[i].user)
+
+                var created_at = formatDate(data[i].created_at);
+                var row = '<tr>' +
+                    '<td>' + created_at + '</td>' +
+                    '<td>' + data[i].no_inv + '</td>' +
+                    '<td>' + (data[i].service_name ? data[i].service_name.charAt(0).toUpperCase() + data[i].service_name.slice(1) : '-') + '</td>' +
+                    '<td>' + data[i].transaction_name + ' - ' + data[i].transaction_desc + '</td>' +
+                    '<td>' + (data[i].payment_method ? data[i].payment_method.replace(/_/g, ' ') : '-') + '</td>' +
+                    '<td>' + (data[i].transaction_price ? 'Rp ' + data[i].transaction_price.toLocaleString('id-ID') : '-') + '</td>' +
+                    '<td>' + data[i].point + '</td>' +
+                    '</tr>';
+
+                tableBody.append(row);
+
+
+            }
+
+            // Inisialisasi DataTable
+                // $('#kt_datatable_zero_configuration').DataTable({
+                //     "scrollY": "500px",
+                //     "scrollCollapse": true,
+                //     "language": {
+                //         "lengthMenu": "Show _MENU_",
+                //     },
+                //     "dom": "<'row'" +
+                //         "<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+                //         "<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+                //         ">" +
+
+                //         "<'table-responsive'tr>" +
+
+                //         "<'row'" +
+                //         "<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+                //         "<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+                //         ">"
+                // });
+        }
+    });
+}
+
+    function formatDate(dateString) {
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        };
+        return new Date(dateString).toLocaleDateString(undefined, options);
+    }
+
+</script>
