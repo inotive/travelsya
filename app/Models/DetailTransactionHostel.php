2211f5c03a12a4fc6c93dd49c4482e42afe14950
Muhammad Irfan Permana <irfanthejoelax@gmail.com>
2023-10-31 09:15:44 +0800

    PTTW-200: [Website Landing] Halaman Landingpage - Hostel - Pembayaran

--- app/Models/DetailTransactionHostel.php
+++ app/Models/DetailTransactionHostel.php
@@ -7,7 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class DetailTransactionHostel extends Model
 {
     public $timestamps = false;
+
     protected $table = 'detail_transaction_hostel';
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
 
     public function hostel()
     {
