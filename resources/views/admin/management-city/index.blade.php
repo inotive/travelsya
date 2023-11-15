a61ddd1fcb599479ccdcac2ebf89b42752cb43fa
ErickOcto <octofrederick@gmail.com>
2023-11-15 06:22:14 +0400

    [Web] Halaman Kelola Kota | br-175

--- resources/views/admin/management-city/index.blade.php
+++ resources/views/admin/management-city/index.blade.php
@@ -5,14 +5,6 @@
 
     <!--begin::Tables Widget 11-->
     <div class="card mb-5 mb-xl-8">
-        <!--begin::Header-->
-        <div class="card-header pt-5">
-            <div class="card-toolbar">
-                <a class="btn btn-sm btn-light-primary" data-bs-toggle="modal" data-bs-target="#create">
-                    <i class="ki-duotone ki-plus fs-2"></i>Tambah Iklan</a>
-            </div>
-        </div>
-        <!--end::Header-->
         <!--begin::Body-->
         <div class="card-body py-3">
             <!--begin::Table container-->
@@ -25,26 +17,81 @@
                         <tr class="fw-bold fs-6 text-gray-800 ">
                             <th class="text-center">No.</th>
                             <th class="text-center">Nama Kota</th>
-                            <th class="text-center">Photo</th>
+                            <th class="min-w-300px text-center">Photo</th>
+                            <th class="text-center">Status</th>
                             <th class="text-center">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($cities as $city)
+                        @foreach ($cities as $key => $city)
                             <tr>
                                 <td>{{ $loop->iteration }}</td>
                                 <td>{{ $city->city_name }}</td>
-                                <td><img width="100px" src="{{ asset('storage/media/kota/'. $city->image) }}" alt="{{ $city->image }}"></td>
+                                <td><img width="150" src="{{ asset('storage/media/kota/'. $city->image) }}" alt="{{ $city->image }}" class="mx-auto"></td>
                                 <td>
-                                    <a href="javascript:void(0)" id="btn-edit-post" data-city_id="{{ $city->city_id }}" class="badge bg-success border-0 text-white">EDIT</a>
-                                    <form action="{{ route('admin.city-management.updateLanding', $city->city_id) }}" method="POST">
+                                    <div class="badge {{ $city->status == 1 ? 'bg-success' : 'bg-danger' }} text-white">
+                                        {{ $city->status == 1 ? 'Aktif' : 'Tidak Aktif' }}
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="text-center d-flex">
+                                    <form id="statusForm_{{ $city->city_id }}" action="{{ route('admin.city-management.updateLanding', $city->city_id) }}" method="POST" class="me-2">
                                         @csrf
                                         @method('put')
                                         <input type="hidden" name="status" value="{{ $city->status == 0 ? 1 : 0 }}">
-                                        <button type="submit" class="badge {{ $city->status == 0 ? 'bg-warning' : 'bg-danger' }} border-0 text-white">
-                                            {{ $city->status == 0 ? 'Tampilkan Di Home' : 'Sembunyikan' }}
+                                        <button id="statusButton_{{ $city->city_id }}" type="button" class="btn btn-sm {{ $city->status == 0 ? 'btn-success' : 'btn-danger' }} border-0 text-white">
+                                            {{ $city->status == 0 ? 'Aktifkan' : 'Nonaktifkan' }}
                                         </button>
                                     </form>
+                                    <a href="javascript:void(0)" id="btn-edit-post" data-city_id="{{ $city->city_id }}" class="btn btn-sm btn-warning border-0 text-white mb-5">EDIT</a>
+                                    </div>
+                                    @php
+                                        $cityStatus = $city->status;
+                                        $kunci = $city->city_id;
+                                    @endphp
+
+                                    @if ($cityStatus == 0)
+                                        <script>
+                                            document.getElementById('statusButton_{{ $kunci }}').addEventListener('click', function() {
+                                                Swal.fire({
+                                                    title: 'Konfirmasi Tampilkan Kota',
+                                                    text: 'Apakah Anda ingin menampilkan kota ini ke landing page?',
+                                                    icon: 'question',
+                                                    showCancelButton: true,
+                                                    confirmButtonColor: '#3085d6',
+                                                    cancelButtonColor: '#d33',
+                                                    confirmButtonText: 'Ya, Tampilkan!',
+                                                    cancelButtonText: 'Batal'
+                                                }).then((result) => {
+                                                    if (result.isConfirmed) {
+                                                        // Jika pengguna mengonfirmasi, kirim formulir
+                                                        document.getElementById('statusForm_{{ $kunci }}').submit();
+                                                    }
+                                                });
+                                            });
+                                        </script>
+                                    @elseif ($cityStatus == 1)
+                                        <script>
+                                            document.getElementById('statusButton_{{ $kunci }}').addEventListener('click', function() {
+                                                Swal.fire({
+                                                    title: 'Konfirmasi Sembunyikan Kota',
+                                                    text: 'Apakah Anda ingin menyembunyikan kota ini dari landing page?',
+                                                    icon: 'question',
+                                                    showCancelButton: true,
+                                                    confirmButtonColor: '#3085d6',
+                                                    cancelButtonColor: '#d33',
+                                                    confirmButtonText: 'Ya, Sembunyikan!',
+                                                    cancelButtonText: 'Batal'
+                                                }).then((result) => {
+                                                    if (result.isConfirmed) {
+                                                        // Jika pengguna mengonfirmasi, kirim formulir
+                                                        document.getElementById('statusForm_{{ $kunci }}').submit();
+                                                    }
+                                                });
+                                            });
+                                        </script>
+                                    @endif
+
                                 </td>
                             </tr>
                         @endforeach
@@ -62,6 +109,7 @@
 
 
 </script>
+
 {{-- Datatables --}}
         <script>
             $(document).ready(function() {
