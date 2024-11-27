eb2f151591ccb7553879042c0b4d0c488f54971a
kuromi <uum1612@gmail.com>
2024-11-27 15:29:45 +0700

    tipis-tipis health_beauty

--- resources/views/pagesv2/health_beauty/index.blade.php
+++ resources/views/pagesv2/health_beauty/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app_v2')
 
 @section('content')
+<div class="container">
     @include('pagesv2.health_beauty.partials._second_nav')
     @include('pagesv2.health_beauty.partials._hero')
     @include('pagesv2.health_beauty.partials._contents')
+</div>
 @endsection
