e47cf47fceefb5a0f722c2b92ad52dc9db65c580
dhaydt <1992dayat@gmail.com>
2024-12-07 10:45:40 +0700

    fix recreations

--- resources/views/pagesv2/rekreasi/components/_categories.blade.php
+++ resources/views/pagesv2/rekreasi/components/_categories.blade.php
@@ -12,7 +12,7 @@
     <div class="panah btn btn-gray-carousel rounded-circle" id="category-button-next">
         <span class="chevron fa-solid fa-chevron-right"></span>
     </div>
-    <a href="#spesial_deals" class="text-danger text-decoration-none"
+    <a href="{{ route('rekreasi.category', ['id' => 'all']) }}" class="text-danger text-decoration-none"
         style="font-size: 1.5rem; font-weight:700; margin-left:auto;">Lihat
         Semua</a>
 </div>
@@ -22,11 +22,11 @@
         @foreach ($categorises as $category)
             <div class="swiper-slide gap-2">
                 <!-- Card -->
-                <a href="javascript:" class="card shadow-sm rounded-4 d-flex flex-row align-items-center"
+                <a href="{{ route('rekreasi.category', ['id' => $category['id']]) }}" class="card shadow-sm rounded-4 d-flex flex-row align-items-center"
                     style="width: 18rem;">
-                    <img src="{{ $category['img'] ?? 'https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?ixid=2yJhcHBfaWQiOjEyMDd9&&fm=jpg' }}"
+                    <img src="{{ asset('storage/' . $category['image']) }}"
                         class="card-img-top" alt="{{ $category['name'] }}"
-                        onerror="this.src='https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?ixid=2yJhcHBfaWQiOjEyMDd9&&fm=jpg'">
+                        onerror="this.src='https://images.unsplash.com/photo-1521289594125-e8e32a214a2b?q=80&w=3787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'">
                     <div class="d-flex flex-column w-100 align-items-center" style="z-index: 1; position: absolute;">
                         <div class="carousel-tab">
                             <h3 class="text-light fw-bold">{{ $category['name'] }}</h3>
@@ -43,7 +43,7 @@
     <script>
         const cswiper = new Swiper('.categories-swiper-container', {
             slidesPerView: 4, // Tampilkan 4 slide sekaligus
-            spaceBetween: 50, // Jarak antar slide
+            spaceBetween: 20, // Jarak antar slide
             navigation: {
                 nextEl: '#category-button-next',
                 prevEl: '#category-button-prev',
