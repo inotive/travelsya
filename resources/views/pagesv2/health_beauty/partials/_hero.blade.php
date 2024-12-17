6a48171df5da0517375afa2795493f5a27b7a3f5
Gusti Bagus Wahyu Saputra <77344072+gusbgs@users.noreply.github.com>
2024-12-17 14:18:52 +0800

    Update _hero.blade.php

--- resources/views/pagesv2/health_beauty/partials/_hero.blade.php
+++ resources/views/pagesv2/health_beauty/partials/_hero.blade.php
@@ -26,13 +26,13 @@
                         </div>
                         <div class="input-group mb-3">
                             @php
-                                $cities = \App\Models\Clinic::all(); // Ubah ke array
-//                                $cityList = \App\Models\City::whereIn('city_id', $cities)->get(); // Query data sesuai ID
+                                $cities = \App\Models\Clinic::pluck('city')->toArray(); // Ubah ke array
+                                $cityList = \App\Models\City::whereIn('city_id', $cities)->get(); // Query data sesuai ID
                             @endphp
                             <select name="location" id="location" class="form-select select" data-control="select2"
                                     data-placeholder="Pilih Lokasi" autocomplete="on">
-                                @foreach($cities as $city)
-                                    <option value="{{ $city->city }}">{{ $city->city }}</option>
+                                @foreach($cityList as $city)
+                                    <option value="{{ $city->city_id }}">{{ $city->city_name }}</option>
                                 @endforeach
                             </select>
                         </div>
