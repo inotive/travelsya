e775b564bf77359491e396d561e4a50d492f7ee4
Fahki Rohandi <fahkirohandi07@gmail.com>
2025-02-19 11:55:28 +0800

    Fix Issue User No. 35

--- resources/views/layouts/include/carousel.blade.php
+++ resources/views/layouts/include/carousel.blade.php
@@ -1,38 +1,47 @@
 @push('add-style')
-<style>
-  .slide {
-    height: 100%;
-    width: 100%
-  }
+    <style>
+        .slide {
+            height: 100%;
+            width: 100%
+        }
 
-  @media (min-width: 860px) {
-    .desktop.carousel {
-      display: block;
-    }
-    .mobile.carousel {
-      display: none;
-    }
-  }
+        @media (min-width: 860px) {
+            .desktop.carousel {
+                display: block;
+            }
 
-  @media (max-width: 860px) {
-    .mobile.carousel {
-      display: block;
-    }
-    .desktop.carousel {
-      display: none;
-    }
-  }
-</style>
+            .mobile.carousel {
+                display: none;
+            }
+        }
+
+        @media (max-width: 860px) {
+            .mobile.carousel {
+                display: block;
+            }
+
+            .desktop.carousel {
+                display: none;
+            }
+        }
+    </style>
 @endpush
 
 <div class="desktop carousel">
     @foreach ($listAds as $ads)
         <div class="slide">
-            <img src="{{ asset('storage/'. $ads->image) }}" class="mw-100 rounded-4" />
+            {{-- <img src="{{ asset('media/ads/' . $ads->image) }}" class="mw-100 rounded-4" width="75" height="75" /> --}}
+            <div class="d-flex justify-content-center align-items-center"
+                style="width: 75px; height: 75px; border: 1px solid #ddd; border-radius: 5px; overflow: hidden;">
+                <a href="{{ $ads->url }}" target="_blank">
+                    <img src="{{ asset('media/ads/' . $ads->image) }}" alt="Dokumentasi"
+                        style="width: 100%; height: auto; object-fit: cover;">
+                </a>
+            </div>
         </div>
     @endforeach
 
-  {{-- <div class="slide">
+    {{-- <div class="slide">
     <img src="{{asset('assets/media/banner-2.jpg')}}" class="mw-100 rounded-4" />
   </div>
   <div class="slide">
@@ -46,10 +55,10 @@
 <div class="mobile carousel">
     @foreach ($listAds as $ads)
         <div class="slide">
-            <img src="{{ asset('storage/'. $ads->image) }}" class="mw-100 rounded-4" />
+            <img src="{{ asset('storage/' . $ads->image) }}" class="mw-100 rounded-4" />
         </div>
     @endforeach
-  {{-- <div class="slide">
+    {{-- <div class="slide">
     <img src="{{asset('assets/media/banner-1.jpg')}}" class="mw-100 rounded-4" />
   </div>
   <div class="slide">
@@ -64,41 +73,40 @@
 </div>
 
 @push('add-script')
-
-<script type="module">
-  var slider = tns({
-    container: '.desktop.carousel',
-    center: true,
-    items: 2,
-    speed: 2000,
-    loop: true,
-    autoplay: true,
-    mouseDrag: true,
-    edgePadding: 10,
-    gutter: 20,
-    autoplayButton: false,
-    nav: false,
-    dots: false,
-    controls: false,
-    autoplayButton: false,
-    autoplayButtonOutput: false,
-  });
-  var slider = tns({
-    container: '.mobile.carousel',
-    center: true,
-    items: 1,
-    speed: 2000,
-    loop: true,
-    autoplay: true,
-    mouseDrag: true,
-    edgePadding: 10,
-    gutter: 20,
-    autoplayButton: false,
-    nav: false,
-    dots: false,
-    controls: false,
-    autoplayButton: false,
-    autoplayButtonOutput: false,
-  });
-  </script>
+    <script type="module">
+        var slider = tns({
+            container: '.desktop.carousel',
+            center: true,
+            items: 2,
+            speed: 2000,
+            loop: true,
+            autoplay: true,
+            mouseDrag: true,
+            edgePadding: 10,
+            gutter: 20,
+            autoplayButton: false,
+            nav: false,
+            dots: false,
+            controls: false,
+            autoplayButton: false,
+            autoplayButtonOutput: false,
+        });
+        var slider = tns({
+            container: '.mobile.carousel',
+            center: true,
+            items: 1,
+            speed: 2000,
+            loop: true,
+            autoplay: true,
+            mouseDrag: true,
+            edgePadding: 10,
+            gutter: 20,
+            autoplayButton: false,
+            nav: false,
+            dots: false,
+            controls: false,
+            autoplayButton: false,
+            autoplayButtonOutput: false,
+        });
+    </script>
 @endpush
