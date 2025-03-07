96a818ae34d4423711526e10fa0d36dc6128b73e
dhaydt <1992dayat@gmail.com>
2025-03-07 22:16:17 +0700

    erro detail rekreasi

--- app/Http/Controllers/NewRecreationController.php
+++ app/Http/Controllers/NewRecreationController.php
@@ -110,6 +110,7 @@ class NewRecreationController extends Controller
     public function detail(Request $request, $id, $date){
         $data['detail'] = Recreation::with('reviews', 'recreationPackages', 'kota')->find($id);
         $data['date'] = $date;
+        $data['current'] = $date;
 
         return view('pagesv2.rekreasi.detail', $data);
     }
