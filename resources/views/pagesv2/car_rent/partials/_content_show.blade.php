841d3d9d4c824afa2e3e87016a248937f7fb8181
dhaydt <1992dayat@gmail.com>
2025-03-11 08:40:24 +0700

    image

--- resources/views/pagesv2/car_rent/partials/_content_show.blade.php
+++ resources/views/pagesv2/car_rent/partials/_content_show.blade.php
@@ -87,8 +87,7 @@
                             <div class="card-body d-flex flex-row">
                                 <img src="{{ asset($car->brand->image ?? null) }}" class="" width="150px" height="100px"
                                     alt="..."
-                                    {{-- onerror="this.src='https://images.unsplash.com/photo-1588440983028-d53e24fa96cc?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'" --}}
-                                    >
+                                    onerror="this.src='https://images.unsplash.com/photo-1588440983028-d53e24fa96cc?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'">
                                 <div class="d-flex flex-column ms-5">
                                     <span class="fw-bold mb-3">{{ $car->brand->name }}</span>
                                     <div class="d-flex flex-row align-items-center">
