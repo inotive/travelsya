9950f2faff0c6a331a8d7ff50eb6dcdaa7b661b6
NoobAtProgramin <144769669+NoobAtProgramin@users.noreply.github.com>
2025-10-01 08:54:09 +0800

    kategori jadi select bus atau travel

--- resources/views/ekstranet/bus-travel/update.blade.php
+++ resources/views/ekstranet/bus-travel/update.blade.php
@@ -27,8 +27,11 @@
             <div class="form-row">
                 <div class="form-group">
                     <label class="required fs-6 fw-semibold mb-2">Kategori</label>
-                    <input type="text" class="form-control form-control-lg" name="kategori"
-                        value="{{ old('kategori', $bus->kategori) }}" placeholder="Masukkan Kategori" />
+                    <select class="form-control form-control-lg" name="kategori">
+                        <option value="">Pilih Kategori</option>
+                        <option value="bus" {{ old('kategori', $bus->kategori) == 'bus' ? 'selected' : '' }}>Bus</option>
+                        <option value="travel" {{ old('kategori', $bus->kategori) == 'travel' ? 'selected' : '' }}>Travel</option>
+                    </select>
                     @error('kategori') <span class="text-danger mt-1">{{ $message }}</span> @enderror
                 </div>
             </div>
