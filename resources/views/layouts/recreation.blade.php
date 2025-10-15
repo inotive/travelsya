c305ebdcbbd95f2113ac1149b090cd0232a92130
JAVIERM2128 <Javiermaretho@gmail.com>
2025-10-15 09:37:32 +0800

    updata icon dan image

--- resources/views/layouts/recreation.blade.php
+++ resources/views/layouts/recreation.blade.php
@@ -17,7 +17,7 @@
     <meta property="og:url" content="" />
     <meta property="og:site_name" content="K" />
     <link rel="canonical" href="" />
-    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+    <link rel="shortcut icon" href="assets/media/logos/_Favicon.ico" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
