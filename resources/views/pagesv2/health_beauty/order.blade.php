fc28aa662a7af13a61b10e21125b63d7834dabb4
JAVIERM2128 <Javiermaretho@gmail.com>
2025-10-20 13:48:34 +0800

    update 1.48

--- resources/views/pagesv2/health_beauty/order.blade.php
+++ resources/views/pagesv2/health_beauty/order.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app_v2')
+@extends('layouts.app_v2_no_main_header')
 
 @section('content')
 <div class="container mb-5">
--- resources/views/pagesv2/health_beauty/partials/_second_nav.blade.php
+++ resources/views/pagesv2/health_beauty/partials/_second_nav.blade.php
@@ -1,4 +1,4 @@
-<div class="card border-transparent header-image" data-bs-theme="light" style="border-radius: 0 !important; margin-bottom: 0 !important; border-bottom-left-radius: 4em !important; border-bottom-right-radius: 4em !important; box-shadow: none !important;">
+<div class="card border-transparent header-image" data-bs-theme="light" style="border-radius: 0 !important; margin-bottom: 0 !important; margin-top: -1px !important; border-bottom-left-radius: 4em !important; border-bottom-right-radius: 4em !important; box-shadow: none !important; margin-left: calc(-1 * (max(100vw - 100%, 0px) / 2)) !important; margin-right: calc(-1 * (max(100vw - 100%, 0px) / 2)) !important;">
     <div class="card-body d-flex ps-xl-20">
         <div class="m-0 w-100">
             <div class="position-relative fs-2x z-index-2 fw-bold text-white mb-2 d-flex justify-content-between align-items-center">
