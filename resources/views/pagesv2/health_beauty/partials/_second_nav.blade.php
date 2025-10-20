846ecdd225a041fb729a4432f643ec403729536d
JAVIERM2128 <Javiermaretho@gmail.com>
2025-10-20 14:04:33 +0800

    update jaga jaga

--- resources/views/pagesv2/health_beauty/partials/_second_nav.blade.php
+++ resources/views/pagesv2/health_beauty/partials/_second_nav.blade.php
@@ -1,14 +1,14 @@
-<div class="card border-transparent header-image" data-bs-theme="light" style="border-radius: 0 !important; margin-bottom: 0 !important; margin-top: -1px !important; border-bottom-left-radius: 4em !important; border-bottom-right-radius: 4em !important; box-shadow: none !important; margin-left: calc(-1 * (max(100vw - 100%, 0px) / 2)) !important; margin-right: calc(-1 * (max(100vw - 100%, 0px) / 2)) !important;">
+<div class="card border-transparent header-image" data-bs-theme="light" style="border-radius: 0 !important; margin-bottom: 0 !important; margin-top: -1px !important; border-bottom-left-radius: 4em !important; border-bottom-right-radius: 4em !important; box-shadow: none !important; margin-left: calc(-1 * (max(100vw - 100%, 0px) / 2)) !important; margin-right: calc(-1 * (max(100vw - 100%, 0px) / 2)) !important; min-height: 200px !important;">
     <div class="card-body d-flex ps-xl-20">
         <div class="m-0 w-100">
-            <div class="position-relative fs-2x z-index-2 fw-bold text-white mb-2 d-flex justify-content-between align-items-center">
+            <div class="position-relative fs-2x z-index-2 fw-bold text-white mb-2 d-flex justify-content-between align-items-center" style="padding-top: 40px;">
                 <button onclick="history.back()"
                     class="btn btn-icon btn-rounded btn-color-white bg-white bg-opacity-15 bg-hover-opacity-25 fw-semibold">
                     <i class="las la-times"></i>
                 </button>
                 <div class="text-center flex-grow-1">
-                    <div>Health & Beauty</div>
-                    <div class="fs-5 text-gray-300">Cari klinik kecantikan dan kesehatan di lokasimu!</div>
+                    <div class="fs-1">Health & Beauty</div>
+                    <div class="fs-4 text-gray-300">Cari klinik kecantikan dan kesehatan di lokasimu!</div>
                 </div>
                 <div></div> <!-- Empty div for spacing symmetry -->
             </div>
